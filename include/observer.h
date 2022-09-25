0aa4e246a11a3b7b7dd95199383bad1628bc0113
peppapighs <ptk.prasertsuk@gmail.com>
2022-09-25 14:40:39 +0800

    :sparkles: add observer class

---
+++ include/observer.h
@@ -0,0 +1,47 @@
+#ifndef INCLUDE_OBSERVER_H_
+#define INCLUDE_OBSERVER_H_
+
+#include "direction.h"
+#include "wave.h"
+
+#include <array>
+#include <tuple>
+#include <vector>
+
+class Observer {
+  private:
+    // Size of the wave
+    const std::size_t row, col, size;
+
+    // Number of patterns
+    const std::size_t num_patterns;
+
+    // Compatibility matrix for patterns
+    const std::vector<std::array<std::vector<std::size_t>, NUM_DIRECTIONS>>
+        compatibility;
+
+    // Number of compatibile patterns in each direction
+    std::vector<std::vector<std::array<std::size_t, NUM_DIRECTIONS>>>
+        compatibility_count;
+
+    // Queue of observed cells
+    std::vector<std::pair<std::size_t, std::size_t>> queue;
+
+  public:
+    // Initialize observer
+    Observer(
+        std::size_t row, std::size_t col,
+        const std::vector<std::array<std::vector<std::size_t>, NUM_DIRECTIONS>>
+            &compatibility);
+
+    // Remove pattern from the wave
+    void remove_pattern(std::size_t index, std::size_t pattern);
+
+    // Remove pattern from the wave
+    void remove_pattern(std::size_t i, std::size_t j, std::size_t pattern);
+
+    // Propagate the observation
+    void propagate(Wave &wave);
+};
+
+#endif
---
+++ src/observer.cpp
@@ -0,0 +1,56 @@
+#include "observer.h"
+#include "direction.h"
+#include "wave.h"
+
+Observer::Observer(
+    std::size_t row, std::size_t col,
+    const std::vector<std::array<std::vector<std::size_t>, NUM_DIRECTIONS>>
+        &compatibility)
+    : row(row), col(col), size(row * col), num_patterns(compatibility.size()),
+      compatibility(compatibility),
+      compatibility_count(
+          size,
+          std::vector<std::array<std::size_t, NUM_DIRECTIONS>>(num_patterns)) {
+    for (std::size_t i = 0; i < size; i++)
+        for (std::size_t pattern = 0; pattern < num_patterns; pattern++)
+            for (std::size_t direction = 0; direction < NUM_DIRECTIONS;
+                 direction++)
+                compatibility_count[i][pattern][direction] =
+                    compatibility[pattern][direction].size();
+}
+
+void Observer::remove_pattern(std::size_t index, std::size_t pattern) {
+    compatibility_count[index][pattern].fill(0);
+    queue.emplace_back(index, pattern);
+}
+
+void Observer::remove_pattern(std::size_t i, std::size_t j,
+                              std::size_t pattern) {
+    remove_pattern(i * col + j, pattern);
+}
+
+void Observer::propagate(Wave &wave) {
+    while (!queue.empty()) {
+        std::pair<std::size_t, std::size_t> current_observation = queue.back();
+        auto &[index, pattern] = current_observation;
+        queue.pop_back();
+
+        long r = index / col, c = index % col;
+        for (std::size_t i = 0; i < NUM_DIRECTIONS; i++) {
+            long nr = r + DX[i], nc = c + DY[i];
+
+            if (nr < 0 || nr >= row || nc < 0 || nc >= col)
+                continue;
+
+            std::size_t nindex = nr * col + nc;
+            for (const std::size_t &pattern : compatibility[pattern][i]) {
+                --compatibility_count[nindex][pattern][OPPOSITE[i]];
+
+                if (compatibility_count[nindex][pattern][OPPOSITE[i]] == 0) {
+                    remove_pattern(nindex, pattern);
+                    wave.remove_pattern(nindex, pattern);
+                }
+            }
+        }
+    }
+}
