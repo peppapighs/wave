11fcdbaa467afca5d9fa980db5de7de3215e56f4
peppapighs <ptk.prasertsuk@gmail.com>
2022-09-25 14:18:30 +0800

    :sparkles: add direction constants

---
+++ include/direction.h
@@ -0,0 +1,14 @@
+#ifndef INCLUDE_DIRECTION_H_
+#define INCLUDE_DIRECTION_H_
+
+#include <array>
+
+const std::size_t NUM_DIRECTIONS = 4;
+
+const std::array<std::size_t, NUM_DIRECTIONS> DX = {1, 0, -1, 0};
+
+const std::array<std::size_t, NUM_DIRECTIONS> DY = {0, 1, 0, -1};
+
+const std::array<std::size_t, NUM_DIRECTIONS> OPPOSITE = {2, 3, 0, 1};
+
+#endif
