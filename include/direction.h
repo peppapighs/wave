2e2a5ee01a14fb7972dd9156334578fb4c4aaaf8
peppapighs <ptk.prasertsuk@gmail.com>
2022-09-25 14:28:14 +0800

    :bug: fix incorrect type for dx & dy

--- include/direction.h
+++ include/direction.h
@@ -5,9 +5,9 @@
 
 const std::size_t NUM_DIRECTIONS = 4;
 
-const std::array<std::size_t, NUM_DIRECTIONS> DX = {1, 0, -1, 0};
+const std::array<int, NUM_DIRECTIONS> DX = {1, 0, -1, 0};
 
-const std::array<std::size_t, NUM_DIRECTIONS> DY = {0, 1, 0, -1};
+const std::array<int, NUM_DIRECTIONS> DY = {0, 1, 0, -1};
 
 const std::array<std::size_t, NUM_DIRECTIONS> OPPOSITE = {2, 3, 0, 1};
 
