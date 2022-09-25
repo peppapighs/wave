e088a0bdf2b0a41fde9b81fb7ddb1ca2acc42193
peppapighs <ptk.prasertsuk@gmail.com>
2022-09-25 14:02:29 +0800

    :sparkles: add wave class

---
+++ include/wave.h
@@ -0,0 +1,49 @@
+#ifndef INCLUDE_WAVE_H_
+#define INCLUDE_WAVE_H_
+
+#include <random>
+#include <vector>
+
+// Noise factor for breaking entropy ties
+const double NOISE_FACTOR = 1E-6;
+
+class Wave {
+  private:
+    // Size of the wave
+    const std::size_t row, col, size;
+
+    // Number of patterns
+    const std::size_t num_patterns;
+
+    // Pattern weights
+    const std::vector<double> weights;
+
+    // Pattern w * log(w)
+    const std::vector<double> w_log_w;
+
+    // Wave data (0 - can place a pattern, 1 - cannot place a pattern)
+    std::vector<std::vector<bool>> data;
+
+    // Sum of weights
+    std::vector<double> sum_weights;
+
+    // Sum of w * log(w)
+    std::vector<double> sum_w_log_w;
+
+    // Entropy of the wave
+    std::vector<double> entropy;
+
+    // Number of remaining patterns in the wave
+    std::vector<std::size_t> remaining_patterns;
+
+  public:
+    Wave(std::size_t row, std::size_t col, const std::vector<double> &weights);
+
+    // Remove pattern from the wave
+    void remove_pattern(std::size_t index, std::size_t pattern);
+
+    // Get the index of the cell with minimum entropy
+    long get_min_entropy(std::mt19937 &rng) const;
+};
+
+#endif
---
+++ src/wave.cpp
@@ -0,0 +1,62 @@
+#include "wave.h"
+
+#include <cmath>
+#include <numeric>
+#include <random>
+#include <vector>
+
+// Compute w * log(w)
+std::vector<double> get_w_log_w(const std::vector<double> &weights) {
+    std::vector<double> w_log_w;
+    for (const double &weight : weights)
+        w_log_w.emplace_back(weight * std::log(weight));
+
+    return w_log_w;
+}
+
+Wave::Wave(std::size_t row, std::size_t col, const std::vector<double> &weights)
+    : row(row), col(col), size(row * col), num_patterns(weights.size()),
+      weights(weights), w_log_w(get_w_log_w(weights)),
+      data(size, std::vector<bool>(num_patterns)),
+      remaining_patterns(size, num_patterns) {
+    double base_w = std::accumulate(weights.begin(), weights.end(), 0);
+    double base_e = std::accumulate(w_log_w.begin(), w_log_w.end(), 0);
+    double base_entropy = std::log(base_w) - base_e / base_w;
+
+    sum_weights = std::vector<double>(size, base_w);
+    sum_w_log_w = std::vector<double>(size, base_e);
+    entropy = std::vector<double>(size, base_entropy);
+}
+
+void Wave::remove_pattern(std::size_t index, std::size_t pattern) {
+    if (data[index][pattern])
+        return;
+
+    sum_weights[index] -= weights[pattern];
+    sum_w_log_w[index] -= w_log_w[pattern];
+    entropy[index] =
+        std::log(sum_weights[index]) - sum_w_log_w[index] / sum_weights[index];
+    --remaining_patterns[index];
+}
+
+long Wave::get_min_entropy(std::mt19937 &rng) const {
+    double min_entropy = std::numeric_limits<double>::max();
+    long min_index = -1;
+
+    std::uniform_real_distribution<> dist(0, 1);
+
+    for (std::size_t i = 0; i < size; i++) {
+        if (remaining_patterns[i] <= 1)
+            continue;
+
+        if (entropy[i] <= min_entropy) {
+            double noise = dist(rng) * NOISE_FACTOR;
+            if (entropy[i] + noise < min_entropy) {
+                min_entropy = entropy[i] + noise;
+                min_index = i;
+            }
+        }
+    }
+
+    return min_index;
+}
